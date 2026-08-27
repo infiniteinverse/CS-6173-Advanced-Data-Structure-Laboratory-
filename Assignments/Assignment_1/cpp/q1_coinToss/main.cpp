5785d24c30a4fb5dc16e7e0a26e33877fa7d2191
infiniteinvere <sumitkarmakarnitdgp@gmail.com>
2026-08-27 11:24:55 +0530

    [asg 1]: basic commit for files

---
+++ Assignments/Assignment_1/cpp/q1_coinToss/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // cout << "Hello, World!" << endl;
+    // return 0;
+
+    mt19937 rng(1234);
+    int N = 5000;
+    uniform_int_distribution<int> dist(0,1);
+
+    ofstream coin("coin_toss.csv");
+
+    if(!coin.is_open()){
+
+    }
+
+
+}
+
